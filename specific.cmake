c3d34e2c8cc4a1ebbe7c900509397b7908bc454c
Wai-Shing Luk <luk036@gmail.com>
2021-07-19 19:42:02 +0800

    try to fix the xtensor build problem (use CPM)

--- specific.cmake
+++ specific.cmake
@@ -1,14 +1,9 @@
-CPMAddPackage("gh:xtensor-stack/xtl#0.7.2")
-CPMAddPackage("gh:xtensor-stack/xtensor#0.23.10")
-
-find_package(xtensor-blas REQUIRED)
-if (xtensor-blas_FOUND)
-  message(STATUS "Found xtensor-blas: ${xtensor-blas_INCLUDE_DIRS}")
-  include_directories(${xtensor-blas_INCLUDE_DIRS})
-endif(xtensor-blas_FOUND)
-
 find_package(OpenBLAS REQUIRED)
 if (OpenBLAS_FOUND)
   message(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
   # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE ${OpenBLAS_INCLUDE_DIRS})
 endif (OpenBLAS_FOUND)
+
+CPMAddPackage("gh:xtensor-stack/xtl#0.7.2")
+CPMAddPackage("gh:xtensor-stack/xtensor#0.21.0")
+CPMAddPackage("gh:xtensor-stack/xtensor-blas#0.18.0")
