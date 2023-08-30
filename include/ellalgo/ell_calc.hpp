0b4358bb2ef5f313cc799014d33505ca506e21cd
Wai-Shing Luk <luk036@gmail.com>
2023-08-30 11:48:19 +0800

    add EllCalcCore

--- include/ellalgo/ell_calc.hpp
+++ include/ellalgo/ell_calc.hpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <tuple>
 
+#include "ell_calc_core.hpp"
+
 // forward declaration
 enum class CutStatus;
 
@@ -18,13 +20,15 @@ class EllCalc {
     bool use_parallel_cut = true;
 
   protected:
-    const double _nFloat;
-    const double _nPlus1;
-    const double _halfN;
-    const double _nSq;
-    const double _c1;
-    const double _c2;
-    const double _c3;
+    const double _n_f;
+    const EllCalcCore _helper;
+
+    // const double _nPlus1;
+    // const double _halfN;
+    // const double _nSq;
+    // const double _c1;
+    // const double _c2;
+    // const double _c3;
 
   public:
     /**
@@ -36,14 +40,13 @@ class EllCalc {
      * @param mq
      * @param x
      */
-    explicit EllCalc(size_t ndim)
-        : _nFloat{double(ndim)},
-          _nPlus1{_nFloat + 1.0},
-          _halfN{_nFloat / 2.0},
-          _nSq{_nFloat * _nFloat},
-          _c1{_nSq / (_nSq - 1.0)},
-          _c2{2.0 / _nPlus1},
-          _c3{_nFloat / _nPlus1} {}
+    explicit EllCalc(size_t ndim) : _n_f{double(ndim)}, _helper{ndim} {}
+    // _nPlus1{_n_f + 1.0},
+    // _halfN{_n_f / 2.0},
+    // _nSq{_n_f * _n_f},
+    // _c1{_nSq / (_nSq - 1.0)},
+    // _c2{2.0 / _nPlus1},
+    // _c3{_n_f / _nPlus1} {}
 
     /**
      * @brief Construct a new EllCalcobject
@@ -144,29 +147,29 @@ class EllCalc {
     auto calc_deep_cut_q(const double &beta, const double &tsq) const
         -> std::tuple<CutStatus, std::tuple<double, double, double>>;
 
-  private:
-    /**
-     * @brief
-     *
-     * @param beta0
-     * @param beta1
-     * @param b1sq
-     * @param b0b1
-     * @param tsq
-     * @return std::tuple<CutStatus, std::tuple<double, double, double>>
-     */
-    auto _calc_parallel_core(const double &beta0, const double &beta1, const double &b1sq,
-                             const double &b0b1, const double &tsq) const
-        -> std::tuple<CutStatus, std::tuple<double, double, double>>;
+    // private:
+    //   /**
+    //    * @brief
+    //    *
+    //    * @param beta0
+    //    * @param beta1
+    //    * @param b1sq
+    //    * @param b0b1
+    //    * @param tsq
+    //    * @return std::tuple<CutStatus, std::tuple<double, double, double>>
+    //    */
+    //   auto _calc_parallel_core(const double &beta0, const double &beta1, const double &b1sq,
+    //                            const double &b0b1, const double &tsq) const
+    //       -> std::tuple<CutStatus, std::tuple<double, double, double>>;
 
-    /**
-     * @brief
-     *
-     * @param beta
-     * @param tau
-     * @param gamma
-     * @return std::tuple<CutStatus, std::tuple<double, double, double>>
-     */
-    auto _calc_deep_cut_core(const double &beta, const double &tau, const double &gamma) const
-        -> std::tuple<CutStatus, std::tuple<double, double, double>>;
+    //   /**
+    //    * @brief
+    //    *
+    //    * @param beta
+    //    * @param tau
+    //    * @param gamma
+    //    * @return std::tuple<CutStatus, std::tuple<double, double, double>>
+    //    */
+    //   auto _calc_deep_cut_core(const double &beta, const double &tau, const double &gamma) const
+    //       -> std::tuple<CutStatus, std::tuple<double, double, double>>;
 };  // } EllCalc
