14794b50e70edc8c5b338c7fd1db4f04454f74d4
Wai-Shing Luk <luk036@gmail.com>
2025-09-17 13:16:04 +0800

    fix windows builds by adding -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -Wno-dev

--- xmake.lua
+++ xmake.lua
@@ -12,9 +12,9 @@ end
 if is_plat("linux") then
     set_warnings("all", "error")
     add_cxflags("-Wconversion", {force = true})
-    add_cxflags("-nostdinc++", {force = true})
-    add_sysincludedirs(os.getenv("PREFIX") .. "/include/c++/v1", {public = true})
-    add_sysincludedirs(os.getenv("PREFIX") .. "/include", {public = true})
+    -- add_cxflags("-nostdinc++", {force = true})
+    -- add_sysincludedirs(os.getenv("PREFIX") .. "/include/c++/v1", {public = true})
+    -- add_sysincludedirs(os.getenv("PREFIX") .. "/include", {public = true})
 elseif is_plat("windows") then
     add_cxflags("/W4 /WX /wd4127 /wd4530", {force = true})
 end
