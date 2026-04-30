750cf2875bd01f833830a6039dd1890361d70b66
Wai-Shing Luk <luk036@gmail.com>
2026-04-30 10:23:29 +0800

    Upgrade fmt 12.1.0 and spdlog 1.17.0 for Termux

--- cmake/specific.cmake
+++ cmake/specific.cmake
@@ -1,13 +1,13 @@
 CPMAddPackage(
   NAME fmt
-  GIT_TAG 10.2.1
+  GIT_TAG 12.1.0
   GITHUB_REPOSITORY fmtlib/fmt
   OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
 CPMAddPackage(
   NAME spdlog
-  GIT_TAG v1.12.0
+  GIT_TAG v1.17.0
   GITHUB_REPOSITORY gabime/spdlog
   OPTIONS "SPDLOG_INSTALL YES" "SPDLOG_FMT_EXTERNAL YES" # Use external fmt to avoid bundled fmt
                                                          # deprecation issues
