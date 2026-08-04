7e92f336c4f64edb89a5b212f8dd8f8e43f777cc
luk036 <luk036@users.noreply.github.com>
2026-08-04 02:48:56 +0000

    deploy: b19ee36b54846efddcd4c08b44c703dce9831d26

--- search/all_7.js
+++ search/all_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['half_5fnonnegative_0',['half_nonnegative',['../namespacealgo.html#a2f44de1a12125a3ae8c0e657fb1f4d29',1,'algo']]],
   ['half_5fnonnegative_2ehpp_1',['half_nonnegative.hpp',['../half__nonnegative_8hpp.html',1,'']]],
-  ['history_2',['Star History',['../index.html#autotoc_md16',1,'']]]
+  ['history_2',['Star History',['../index.html#autotoc_md15',1,'']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -6,5 +6,5 @@ var searchData=
   ['profitoracle_3',['profitoracle',['../classProfitOracle.html',1,'ProfitOracle'],['../classProfitOracle.html#aa642df03dc11a8d086ce8b1e652ebc6f',1,'ProfitOracle::ProfitOracle(double p, double A, double k, const Vec &amp;a, const Vec &amp;v)'],['../classProfitOracle.html#a258ec5f52763e1fce40827129a0f48f1',1,'ProfitOracle::ProfitOracle(const ProfitOracle &amp;)=delete'],['../classProfitOracle.html#a107423f00e0549cd67ce1b4e864e05a9',1,'ProfitOracle::ProfitOracle(ProfitOracle &amp;&amp;)=delete']]],
   ['profitoracleq_4',['profitoracleq',['../classProfitOracleQ.html',1,'ProfitOracleQ'],['../classProfitOracleQ.html#a6848fe2aaae2853611d1ae5342d327e2',1,'ProfitOracleQ::ProfitOracleQ()']]],
   ['profitoraclerb_5',['profitoraclerb',['../classProfitOracleRb.html',1,'ProfitOracleRb'],['../classProfitOracleRb.html#afbb320fd145c8319af423a1e063d8b80',1,'ProfitOracleRb::ProfitOracleRb()']]],
-  ['projects_20and_20alternatives_6',['Related projects and alternatives',['../index.html#autotoc_md15',1,'']]]
+  ['projects_20and_20alternatives_6',['Related projects and alternatives',['../index.html#autotoc_md14',1,'']]]
 ];
